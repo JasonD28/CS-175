2bccb7c0059381c1c12ea2efa013220d9d77e784
shot4213 <shot4213@gmail.com>
2020-06-05 02:02:00 -0700

    Add files via upload

--- model/dataset.py
+++ model/dataset.py
@@ -9,14 +9,15 @@ import sys
 from torchvision import transforms
 
 class PneumoniaDataset(Dataset):
-    def __init__(self, root, transforms):
+    def __init__(self, root, transforms, mode='train'):
         self.root = root
         self.transforms = transforms
-        self.imgs = list(sorted(os.listdir(os.path.join(root, "stage_2_train_images"))))
-        self.labels = pd.read_csv(os.path.join(root, "stage_2_train_labels.csv"))
+        self.mode = mode
+        self.imgs = list(sorted(os.listdir(os.path.join(root, "stage_2_"+self.mode+"_images"))))
+        self.labels = pd.read_csv(os.path.join(root, self.mode+".csv"))
 
     def __getitem__(self, index):
-        img_path = os.path.join(self.root, "stage_2_train_images", self.imgs[index])
+        img_path = os.path.join(self.root, "stage_2_"+self.mode+"_images", self.imgs[index])
         dicom = pydicom.read_file(img_path)
         image = dicom.pixel_array
         image = image[..., np.newaxis]
@@ -29,26 +30,29 @@ class PneumoniaDataset(Dataset):
             'image_id': torch.tensor([index]),
             'iscrowd': torch.zeros((1,), dtype=torch.int64)
         }
+        if self.mode == 'train':
+            if int(img_label['Target']) == 1:
+                width = int(img_label['width'])
+                height = int(img_label['height'])
+                x = int(img_label['x'])
+                y = int(img_label['y'])
 
-        if int(img_label['Target']) == 1:
-            width = int(img_label['width'])
-            height = int(img_label['height'])
-            x = int(img_label['x'])
-            y = int(img_label['y'])
+                boxes = [x, y, x + width, y + height]
+                boxes = torch.tensor([boxes], dtype=torch.float32)
 
-            boxes = [x, y, x + width, y + height]
-            boxes = torch.tensor([boxes], dtype=torch.float32)
+                labels = torch.tensor([2], dtype=torch.int64)
 
-            labels = torch.ones((1,), dtype=torch.int64)
-
-            target['boxes'] = boxes
-            target['labels'] = labels
-            target['area'] = torch.tensor([height * width], dtype=torch.int64)
-        else:
-            target['boxes'] = torch.tensor([[0,1,2,3]], dtype=torch.float32)
-            target['labels'] = torch.zeros((1,), dtype=torch.int64)
-            target['area'] = torch.tensor([4], dtype=torch.int64)
-        return image, target
+                target['boxes'] = boxes
+                target['area'] = torch.tensor([height * width], dtype=torch.int64)
+                target['labels'] = labels
+            else:
+                target['boxes'] = torch.tensor([[0,1,2,3]], dtype=torch.float32)
+                target['area'] = torch.tensor([4], dtype=torch.int64)
+                if img_label['class'] == 'Normal':
+                    target['labels'] = torch.tensor([0], dtype=torch.int64)
+                else:
+                    target['labels'] = torch.tensor([1], dtype=torch.int64)
+            return image, target
 
     def __len__(self):
         return len(self.imgs)
